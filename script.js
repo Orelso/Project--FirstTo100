1b1e843c43ad4f5e71277619971fc54293355ad3
Orelso Vitam <orelso.vitam@avanio.com>
2023-02-22 09:13:25 +0200

    first

--- script.js
+++ script.js
@@ -2,6 +2,35 @@
 
 const score0El = document.querySelector('#score--0') 
 const score1El = document.getElementById('score--1')
+const current0El = document.getElementById('current--0')
+const current1El = document.getElementById('current--1')
+const diceEl = document.querySelector('.dice') 
+const btnNew = document.querySelector('.btn--new')
+const btnRoll = document.querySelector('.btn--roll')
+const btnHold = document.querySelector('.btn--hold')
 
+// Starting conditions
 score1El.textContent = 0
 score0El.textContent = 0
+diceEl.classList.add('hidden')
+
+const scores = [0,0]
+let currentScore = 0
+let activePlayer = 0
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function() {
+    // Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1
+    // Display Dice
+    diceEl.classList.remove('hidden')
+    diceEl.src = `dice-${dice}.png`
+    // Check for rolled 1: if true, switch to the next player
+    if(dice !== 1){
+        currentScore += dice
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore
+        current0El.textContent = currentScore
+    } else {
+
+    }
+})
